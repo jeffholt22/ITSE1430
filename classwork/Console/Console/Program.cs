289133f852ce73f61ab5487261a7d17321d85a94
Jeffrey Holt <jeffrey.holt623@my.tccd.edu>
2022-08-24 21:36:18 -0500

    Hello world and Section 1

---
+++ classwork/Console/Console/Program.cs
@@ -0,0 +1,16 @@
+﻿int hours;
+
+Console.WriteLine("Hours: ");
+string value = Console.ReadLine();
+
+hours = Int32.Parse(value);
+
+Console.WriteLine("Pay rate: ");
+value = Console.ReadLine();
+
+double payRate = Double.Parse(value);
+
+Console.WriteLine("Your pay is " + (hours * payRate)); 
+
+
+
